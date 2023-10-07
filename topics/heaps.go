45801c80e16a9d8fe54f9de54340fa8349313b7d
amirhasanzadehpy <amirhasanzadehpy@gmail.com>
2023-10-07 14:02:45 +0330

    add extract max to heap

--- topics/heaps.go
+++ topics/heaps.go
@@ -1,6 +1,8 @@
 package topics
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type heaps struct {
 	values []int
@@ -27,5 +29,53 @@ func (h *heaps) bubbleUp() {
 func (h *heaps) Insert(value int) {
 	h.values = append(h.values, value)
 	h.bubbleUp()
+}
+func (h *heaps) ExtractMax() int {
+	max := h.values[0]
+	end := h.values[len(h.values)-1]
+	// pop
+	h.values = h.values[:len(h.values)-1]
+	if len(h.values) > 0 {
+		h.values[0] = end
+		h.sinkDown()
+	}
+
+	return max
+}
+
+func (h *heaps) Print() {
 	fmt.Println(h.values)
+}
+
+func (h *heaps) sinkDown() {
+	idx := 0
+	element := h.values[0]
+	length := len(h.values)
+	for {
+		leftChildIdx := 2*idx + 1
+		rightChildIdx := 2*idx + 2
+		var leftChild, rightChild int
+		var swap int
+
+		if leftChildIdx < length {
+			leftChild = h.values[leftChildIdx]
+			if element < leftChild {
+				swap = leftChildIdx
+			}
+		}
+
+		if rightChildIdx < length {
+			rightChild = h.values[rightChildIdx]
+			if swap == 0 && element < rightChild || swap != 0 && rightChild > leftChild {
+				swap = rightChildIdx
+			}
+		}
+
+		if swap == 0 {
+			break
+		}
+
+		h.values[idx], h.values[swap] = h.values[swap], h.values[idx]
+		idx = swap
+	}
 }
